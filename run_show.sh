022759a6f1d38f99adb4171642c2aba7b7b50401
Zacharia Crumley <zcrumley@tesla.cs.uct.ac.za>
2011-10-18 13:47:55 +0200

    Fixed a bug where the run_show.sh script did not have a default axiom argument, and therefore failed when run with no axiom supplied.

--- run_show.sh
+++ run_show.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #A script to run the python interpreter using standard arguments.
 
-python interpreter.py -i input -m 5 -p -v $@ | less -S
+python interpreter.py -i input -m 5 -p -v -x enterprise $@ | less -S
 
